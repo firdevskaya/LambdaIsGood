890cbc32b058e3543f63f3e7b75086a1c07f9fe2
Firdevs KAYA <74245732+firdevskaya@users.noreply.github.com>
2022-03-18 17:35:03 +0300

    Update Lambda01.java

--- src/test/java/Lambda01.java
+++ src/test/java/Lambda01.java
@@ -8,10 +8,10 @@ public class Lambda01 {
     public static void main(String[] args) {
 
 
-         /*    2) "Q01" olarak adlandırılan bir class oluşturun
-            3) 5 String öğesi içeren bir String listoluşturun.
-            4) 3 tanesi "A" harfi ile başlayacak
-            5) Elemanlar "A" harfi ile başlıyorsa ve elemanların uzunlukları
+         /*    
+             5 String öğesi içeren bir String listoluşturun.
+             3 tanesi "A" harfi ile başlayacak
+             Elemanlar "A" harfi ile başlıyorsa ve elemanların uzunlukları
             5'ten küçükse, konsolda list olarak buyuk harflerle yazdırın.
 
 
